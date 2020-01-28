493fae2ff4f2c270b2e45f2aca65065bbdc8a8d1
Yoshihiro Togawa <yoshi_jplk@Yoshi-JPLK-Mac.local>
2020-01-28 19:00:29 +0900

    プロトタイプ宣言をして、main関数内で関数を使用する際の引数の扱いが間違えていたので、そこを修正。

---
+++ 練習問題11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int check(int);
+
+int main(void)
+{
+    int year;
+    printf("西暦年を入力してください：");
+    scanf("%d",&year);
+
+    check(int);
+
+    return 0;
+}
+
+int check(int year)
+{
+    if(year % 4 == 0){
+        printf("%d年は夏季オリンピックが開催されました。\n",year);
+    }else if(year % 2 == 0){
+        printf("%d年は冬季オリンピックが開催されました。\n",year);
+    }else{
+        printf("%d年はオリンピックが開催されていません。\n",year);
+    }
+    return 0;
+}
