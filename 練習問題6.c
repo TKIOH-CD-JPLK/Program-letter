3bd3dc2ec5b7873435af2608135a9cff22f05e0c
Yoshihiro Togawa <yoshi_jplk@Yoshi-JPLK-Mac.local>
2020-01-28 15:42:50 +0900

    変数priceに定価を入力することを伝えるための、printf関数を記述。

--- 練習問題6.c
+++ 練習問題6.c
@@ -3,6 +3,7 @@
 int main(void)
 {
     int price;
+    printf("定価を入力してください：");
     scanf("%d",&price);
     int rate1 = (int)(price * 0.9),rate2 = (int)(price * 0.7),rate3 = (int)(price * 0.5),rate4 = (int)(price * 0.2);
     printf("1割引の値段:%d円\n3割引の値段:%d円\n5割引の値段:%d円\n8割引の値段:%d円\n",rate1,rate2,rate3,rate4);
