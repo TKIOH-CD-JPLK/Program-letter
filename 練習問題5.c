7dde0e5858bb7956d82da7d1181f18bb4b44a799
Yoshihiro Togawa <yoshi_jplk@Yoshi-JPLK-Mac.local>
2020-01-28 15:19:40 +0900

    結果を確認するために解答をコピペ

--- 練習問題5.c
+++ 練習問題5.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main(void)
@@ -5,4 +6,25 @@ int main(void)
     int x = 198,y = 138;
     printf("%d円\n",(int)(1000-(198+138*2)*1.05));
     return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int juice,milk,money,payment,change;
+	double tax;
+	
+	juice = 198;
+	milk = 138;
+	money = 1000;
+	tax = 1.05;
+	
+	payment = (int)((juice + milk*2)*tax);
+	change = money - payment;
+	
+	printf("%d円\n",change);
+	
+	return 0;
 }
