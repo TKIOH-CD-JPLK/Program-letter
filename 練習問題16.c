c9543d1930a9f81d8e1a88ceb491f50656215c3d
Yoshihiro Togawa <yoshi_jplk@Yoshi-JPLK-Mac.local>
2020-01-30 17:54:14 +0900

    解答を見て、構造体を配列かすれば二重配列にしないで済むことに気付き、直し

--- 練習問題16.c
+++ 練習問題16.c
@@ -2,26 +2,26 @@
 
 typedef struct 
 {
-    char name[16][256];
-    int age[16];
-    int sex[16];
+    char name[256];
+    int age;
+    int sex;
 } per_info;
 
 void per_info_print(per_info *data);
 
 int main(void)
 {
-    per_info data;
+    per_info data[];
     int i;
 
     for ( i = 0; i < 3; i++)
     {
         printf("名前を入力してください：");
-        scanf("%s\n",data.name[i]);
+        scanf("%s\n",data[i].name[256]);
         printf("年齢を入力してください：");
-        scanf("%d\n",&data.age[i]);
+        scanf("%d\n",&data[i].age);
         printf("性別（1:男性、2:女性）を入力してください：");
-        scanf("%d\n",&data.sex[i]);
+        scanf("%d\n",&data[i].sex);
 
     }
 
@@ -35,9 +35,9 @@ void per_info_print(per_info *data)
     int i;
     for ( i = 0; i < 3; i++)
     {
-        printf("%s\n",data->name[i]);
-        printf("%d\n",data->age[i]);
-        printf("%d\n",data->sex[i]);
+        printf("%s\n",data[i]->name[256]);
+        printf("%d\n",data[i]->age);
+        printf("%d\n",data[i]->sex);
     }
     return;
 }
