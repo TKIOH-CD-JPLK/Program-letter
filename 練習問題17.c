64e12a27fbd0e6314c070c575e37d0e0a7034fa0
Yoshihiro Togawa <yoshi_jplk@Yoshi-JPLK-Mac.local>
2020-01-30 21:08:14 +0900

    とりあえずprintf関数でcsvに対応するデータを表示させる

---
+++ 練習問題17.c
@@ -0,0 +1,55 @@
+/*
+番号,名前,テストの平均点,\n,
+1,野比のび太,0,\n,
+2,源静香,90,\n,
+3,剛田武,40,\n,
+4,骨川スネ夫,70,\n
+*/
+
+#include <stdio.h>
+
+typedef struct 
+{
+    int number;
+    char name[256];
+    int score;
+
+} People ;
+
+void Input_People(People *data);
+void Output_People(People data);
+
+int main(void)
+{
+    int i;
+    People data[4];
+
+    for ( i = 0; i < 4; i++)
+    {
+        Input_People(&data[i]);
+    }
+
+    for ( i = 0; i < 4; i++)
+    {
+        Output_People(data[i]);
+    }
+    
+    return 0;
+}
+
+void Input_People(People *data)
+{
+    printf("番号：");
+    scanf("%d",&data->number);
+    printf("名前：");
+    scanf("%s",data->name);
+    printf("平均点：");
+    scanf("%d",&data->score);
+    return;
+}
+
+void Output_People(People data)
+{
+    printf("%d,%s,%d,",data.number,data.name,data.score);
+    printf("\n");
+}
