b36bd2afba5801ace5096116bfd06c56dad7b0d4
Yoshihiro Togawa <yoshi_jplk@Yoshi-JPLK-Mac.local>
2020-01-30 18:55:32 +0900

    構造体においてポインタ変数を引数にする時の二段階のモードの切り替えがイメージできずに試行錯誤

--- 練習問題16.c
+++ 練習問題16.c
@@ -1,4 +1,4 @@
-/*
+
 #include <stdio.h>
 #include <string.h>
 
@@ -14,7 +14,7 @@ void Output_per_info(per_info date);
 
 int main(void)
 {
-    per_info[3];
+    per_info data[3];
     int i;
 
     for ( i = 0; i < 3; i++)
@@ -32,14 +32,13 @@ int main(void)
 
 void Input_per_info(per_info *data)
 {
-    pdata = &data;
 
     printf("名前：");
     scanf("%s",data->name);
     printf("年齢：");
-    scanf("%d",pdata->age);
+    scanf("%d",&data->age);
     printf("性別（1-男性、2-女性）：");
-    scanf("%d",pdata->sex);
+    scanf("%d",&data->sex);
     printf("\n");
 
 }
@@ -48,8 +47,8 @@ void Output_per_info(per_info data)
 {
     char sex[16];
 
-    printf("名前：%s,\n",data.name);
-    printf("年齢：%d,\n",data.age);
+    printf("名前：%s\n",data.name);
+    printf("年齢：%d\n",data.age);
 
     if (data.sex == 1)
     {
@@ -59,62 +58,5 @@ void Output_per_info(per_info data)
     }
     
     printf("性別：%s\n",sex);
-    
-}
-*/
-
-#include <stdio.h>
-#include <string.h>
-
-typedef struct {
-	char name[256];
-	int age;
-	int sex;
-} People;
-
-void InputPeople(People *data);
-void ShowPeople(People data);
-
-int main(void)
-{
-	People data[3];
-	int i;
-	
-	for (i = 0;i < 3;i++) {
-		InputPeople(&data[i]);
-	}
-	
-	for (i = 0;i < 3;i++) {
-		ShowPeople(data[i]);
-	}
-	
-	return 0;
-}
-
-void InputPeople(People *data)
-{
-	printf("名前:");
-	scanf("%s",data->name);
-	printf("年齢:");
-	scanf("%d",&data->age);
-	printf("性別(1-男性、2-女性):");
-	scanf("%d",&data->sex);
-	printf("\n");
-}
-
-void ShowPeople(People data)
-{
-	char sex[16];
-	
-	printf("名前:%s\n",data.name);
-	printf("年齢:%d\n",data.age);
-	
-	if (data.sex == 1) {
-		strcpy(sex,"男性");
-	} else {
-		strcpy(sex,"女性");
-	}
-	
-	printf("性別:%s\n",sex);
-	printf("\n");
+    printf("\n");
 }
